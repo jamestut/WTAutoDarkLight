2835edcb4356d1a0a727c0874c337006c6526cb6
James <aslan.jnn@gmail.com>
2020-03-19 20:00:51 +1100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # WTAutoDarkLight
-Automatically switch Windows Terminal color scheme based on Windows dark or light settings.
+This app is a stopgap solution for automatically swithing Windows Terminal's color scheme based on Windows dark or light settings, since the Windows Terminal in the current iteration is unable to automatically switch between dark or light mode.
+
+This app has to be kept run. A scheduled task can be configured for this app to run on logon.
+
+This app depends on `Newtonsoft.Json`.
+
+**Warning**: this app will remove the comments in the target Windows Terminal's JSON file!
+
+## Usage
+
+This app takes two arguments:
+
+1. The first argument is the path to the target Windows Terminal's JSON config file. This file will be modified upon app startup and when the app dark or light mode is changed in Windows appearance settings.
+2. The second argument is the JSON file used as the reference theme settings. Please refer to the [Scheme File](#scheme-file) section for the format.
+
+This app will continue to run until stopped, while also watching any changes to the user's app color preference. During this app's startup, or when the app color preference is changed, some values in the configuration file specified in the first argument in the `defaults` object inside the `profiles` object will be replaced with the data specified in the second argument (the [Scheme File](#scheme-file)).
+
+## Scheme File
+
+The scheme file must be a valid JSON file consisting of an object with two key: `dark` and `light`. Those corresponds to dark mode and light mode, respectively.
+
+Each of those keys must be a single level object, with the keys as specified in the below table and all values must be in string.
+
+| Key                   | Valid Value                             | Description                                  |
+| --------------------- | --------------------------------------- | -------------------------------------------- |
+| `colorScheme`         | A valid color scheme name.              | The color scheme that will be used.          |
+| `cursorColor`         | HTML hex triplet color (e.g. `#f0f8ff`) | The color of the cursor.                     |
+| `selectionBackground` | HTML hex triplet color (e.g. `#f0f8ff`) | The background color of selection highlight. |
+
+All keys specified in the above table are optional. If they're not specified, then the Windows Terminal's or color scheme's defaults will be used.
+
+Here is an example of the configuration JSON file:
+
+```json
+{
+    "dark": {
+        "colorScheme":"One Half Dark"
+    },
+    "light": {
+        "colorScheme": "One Half Light",
+        "cursorColor": "#333333",
+        "selectionBackground": "#333333"
+    }
+}
+```
+
---
+++ TerminalAutoDarkLight/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace TerminalAutoDarkLight
+{
+    static class Program
+    {
+        [DllImport("Advapi32.dll")] static extern UInt32 RegNotifyChangeKeyValue(UIntPtr hkey, UInt32 watchSubtree, UInt32 notifyFilter, UIntPtr handler, UInt32 async);
+        [DllImport("Advapi32.dll")] static extern UInt32 RegOpenKeyExA(UIntPtr hkey, string subkey, UInt32 options, UInt32 samDesired, ref UIntPtr result);
+        [DllImport("Advapi32.dll")] static extern UInt32 RegQueryValueExA(UIntPtr hkey, string valueName, UIntPtr reserved, ref UInt32 dwType, ref UInt32 data, ref UInt32 dataSize);
+        [DllImport("user32.dll")] static extern UInt32 MessageBoxA(UIntPtr hwnd, string text, string caption, UInt32 type);
+
+        static string _ConfigPath;
+        static string _MyConfigPath;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                MessageBoxA((UIntPtr)0, "Usage: TerminalAutoDarkLight (windows terminal config path) (scheme path)", "Help", 0);
+                return;
+            }
+            _ConfigPath = args[0];
+            _MyConfigPath = args[1];
+
+            UIntPtr HKCU = (UIntPtr)0x80000001;
+            bool lightMode = false;
+
+            UIntPtr hkeyPersona = (UIntPtr)0;
+            if(RegOpenKeyExA(HKCU, @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", 0, 0x20019, ref hkeyPersona) != 0)
+            {
+                MessageBoxA((UIntPtr)0, "Cannot open registry key", "Error", 0);
+                return;
+            }
+
+            // set the initial mode on startup
+            UInt32 regType = 0, regValue = 0, regValueLen = 4;
+            if(RegQueryValueExA(hkeyPersona, "AppsUseLightTheme", (UIntPtr)0, ref regType, ref regValue, ref regValueLen) != 0)
+            {
+                MessageBoxA((UIntPtr)0, "Cannot read color scheme registry value", "Error", 0);
+                return;
+            }
+            lightMode = regValue != 0;
+            ChangeColorScheme(lightMode);
+
+            
+            while(RegNotifyChangeKeyValue(hkeyPersona, 0, 4, (UIntPtr)0, 0) == 0)
+            {
+                // read app theme setting
+                UInt32 err = RegQueryValueExA(hkeyPersona, "AppsUseLightTheme", (UIntPtr)0, ref regType, ref regValue, ref regValueLen);
+                if (err == 0)
+                {
+                    bool newLightMode = regValue != 0;
+                    if(lightMode != newLightMode)
+                    {
+                        lightMode = newLightMode;
+                        ChangeColorScheme(lightMode);
+                    }
+                }
+            }
+        }
+
+        static void ChangeColorScheme(bool lightMode)
+        {
+            try
+            {
+                var targetObj = JsonConvert.DeserializeObject<IDictionary<string, object>>(File.ReadAllText(_ConfigPath), new DictionaryConverter());
+                var myObj = JsonConvert.DeserializeObject<IDictionary<string, object>>(File.ReadAllText(_MyConfigPath), new DictionaryConverter());
+                myObj = (IDictionary<string,object>)myObj[lightMode ? "light" : "dark"];
+                string[] targets = new string[]{ "profiles", "defaults" };
+                string[] properties = new string[] { "colorScheme", "cursorColor", "selectionBackground" };
+                var currObj = targetObj;
+                foreach(var target in targets)
+                {
+                    if(!currObj.ContainsKey(target))
+                    {
+                        currObj.Add(target, new Dictionary<string, object>());
+                    }
+                    currObj = (Dictionary<string, object>)currObj[target];
+                }
+
+                // replace properties
+                foreach(var prop in properties)
+                {
+                    if(!myObj.ContainsKey(prop))
+                    {
+                        currObj.Remove(prop);
+                    }
+                    else
+                    {
+                        currObj[prop] = myObj[prop];
+                    }
+                }
+                
+                File.WriteAllText(_ConfigPath, JsonConvert.SerializeObject(targetObj, Formatting.Indented));
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
